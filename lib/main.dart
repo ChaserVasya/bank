9395c61a20dc5af38c6cf1b0c1265a9b1273d6dc
Isaikin Vasily <v.chaser@mail.ru>
2022-07-17 01:36:08 +1000

    remove app logo with supposedly invisible layer

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:bank/application/app.dart';
 import 'package:bank/application/error/error_handler.dart';
 
-//TODO Fix. Splash icon is not png! It has visible transparant layout!
 //TODO Feature. Add icon switching for different platforms
 void main() {
   runZonedGuarded(() {
